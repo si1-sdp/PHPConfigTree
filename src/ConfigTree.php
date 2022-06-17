0c1a0dfdfab200f50247f4ce6be75d983f229b25
jm.gervais <jean-marie.gervais@dgfip.finances.gouv.fr>
2022-06-17 17:19:26 +0200

    initial commit

---
+++ src/ConfigTree.php
@@ -0,0 +1,211 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PhpConfigTree
+ */
+
+namespace DgfipSI1\ConfigTree;
+
+use Composer\Json\JsonValidationException;
+use JsonSchema\Validator;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * class RepoMirror
+ * Yum repo mirror class
+ */
+class ConfigTree
+{
+    /** @var array<string,mixed> */
+    protected $options = [];
+
+    /** @var array<string,mixed> */
+    protected $schema = null;
+    /**
+     * Constructor
+     *
+     * @param string              $schemaFile
+     * @param array<string,mixed> $options
+     *
+     * @return void
+     */
+    public function __construct($schemaFile, $options = [])
+    {
+        try {
+            $schemaFileContent = file_get_contents($schemaFile);
+        } catch (\exception $e) {
+            throw new JsonValidationException(sprintf("Schema file not found : '%s'", $schemaFile));
+        }
+        switch (pathinfo($schemaFile, PATHINFO_EXTENSION)) {
+            case 'json':
+                $this->schema  = (array) json_decode("$schemaFileContent", true);
+                break;
+            case 'yml':
+            case 'yaml':
+                $this->schema  = (array) yaml::parseFile($schemaFile, yaml::DUMP_OBJECT_AS_MAP);
+                break;
+        }
+        $this->options = $this->getDefaultOptions();
+        if ($options) {
+            $this->merge($options);
+        }
+    }
+    /**
+     * Undocumented function
+     *
+     * @param array<string,mixed> $options
+     * @param array<string,mixed> $properties
+     *
+     * @return array<string,mixed>
+     */
+    public function getDefaultOptions(&$options = null, &$properties = null)
+    {
+        if (null === $options) {
+            $options = [];
+        }
+        if (null === $properties) {
+            $properties = $this->schema['properties'];
+        }
+        if (!is_array($properties)) {
+            throw new \Exception('Bad schema : properties should be an array');
+        }
+        foreach ($properties as $key => $props) {
+            if (!is_array($props)) {
+                throw new \Exception(sprintf('Bad schema : expecting attributes for property %s', $key));
+            }
+            if (array_key_exists('default', $props)) {
+                $type = $props['type'];
+                if (is_array($type)) {
+                    $type = $type[0];
+                }
+                switch ($type) {
+                    case 'string':
+                        $value = $props['default'];
+                        break;
+                    case 'null':
+                        $value = null;
+                        break;
+                    case 'boolean':
+                        $value = $props['default'] ? true : false;
+                        break;
+                    case 'integer':
+                        $value = 0 + $props['default'];
+                        break;
+                    case 'array':
+                        $value = [];
+                        break;
+                    default:
+                        throw new \Exception(sprintf("Unknown type : '%s'", $type));
+                }
+                $options[$key] = $value;
+            }
+            if (array_key_exists('properties', $props)) {
+                $this->getDefaultOptions($options[$key], $props['properties']);
+            }
+        }
+
+        return $options;
+    }
+    /**
+     * Check properties against shema
+     *
+     * @return bool
+     */
+    public function check()
+    {
+        $validator = new Validator();
+        $validator->check($this->options, $this->schema);
+        if (!$validator->isValid()) {
+            $errors = [];
+            foreach ((array) $validator->getErrors() as $error) {
+                $errors[] = ($error['property'] ? $error['property'].' : ' : '').$error['message'];
+            }
+            throw new JsonValidationException('Validation error: config does not match schema.', $errors);
+        }
+
+        return true;
+    }
+    /**
+     * getters
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function get(string $name)
+    {
+        $branch = &$this->options;
+        $nodes = explode('.', $name);
+        $index = 1;
+        $branchName = '';
+        foreach ($nodes as $node) {
+            if (!array_key_exists($node, $branch)) {
+                if (sizeof($nodes) !== $index) {
+                    throw new \Exception(sprintf("Config branch '%s/%s' does not exist.", $branchName, $node));
+                }
+                throw new \Exception(sprintf("Option '%s' does not exists or has not been set.", $name));
+            }
+            if (sizeof($nodes) !== $index) {
+                $index++;
+                $branchName = "$branchName/$node";
+                /** @var array<string,mixed> $branch */
+                $branch = &$branch[$node];
+            } else {
+                return $branch[$node];
+            }
+        }
+    }
+    /**
+     * Setters
+     *
+     * @param string $name
+     * @param mixed  $value
+     * @param bool   $check
+     *
+     * @return self
+     */
+    public function set(string $name, mixed $value, $check = true)
+    {
+        $branch = &$this->options;
+        $nodes = explode('.', $name);
+        $index = 1;
+        // print "\n\nSET $name => $value\n";
+        // print_r($branch);
+        // print "TYPE : ".gettype($branch)."\n\n";
+        $branchName = '';
+        foreach ($nodes as $node) {
+            if (sizeof($nodes) !== $index) {
+                if (!array_key_exists($node, $branch)) {
+                    throw new \Exception(sprintf("Config branch '%s/%s' does not exist.", $branchName, $node));
+                }
+                /** @var array<string,mixed> $branch */
+                $branch = &$branch[$node];
+            } else {
+                $branch[$node] = $value;
+            }
+            $index++;
+            $branchName = "$branchName/$node";
+        }
+        if ($check) {
+            $this->check();
+        }
+
+        return $this;
+    }
+    /**
+     * merge options given to target array
+     *
+     * @param array<string,mixed> $options
+     *
+     * @return void
+    */
+    public function merge($options)
+    {
+        foreach ($options as $optName => $optValue) {
+            $this->set($optName, $optValue, false);
+        }
+        $this->check();
+    }
+}
---
+++ tests/phpunit/src/ConfigTreeTest.php
@@ -0,0 +1,158 @@
+<?php
+/*
+ * This file is part of DgfipSI1\ConfigTree.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DgfipSI1\ConfigTreeTests;
+
+use Composer\Json\JsonValidationException;
+use DgfipSI1\ConfigTree\ConfigTree;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers DgfipSI1\ConfigTree\ConfigTree
+ */
+class ConfigurationTreeTest extends TestCase
+{
+    /** @var string */
+    private $testDataDir;
+    /** setup a VfsStream filesystem with /conf/satis_dgfip.yaml
+     *
+     * {@inheritDoc}
+     *
+     * @see \PHPUnit\Framework\TestCase::setUp()
+     */
+    protected function setUp(): void
+    {
+        $this->testDataDir = __DIR__."/../data/";
+    }
+   /**
+     * Test execCommand method
+     *
+     */
+    public function testConstructor(): void
+    {
+        /** test yaml and json files for schema */
+        $ctJson = new ConfigTree($this->testDataDir."testSchema.json");
+        $ctYaml = new ConfigTree($this->testDataDir."testSchema.yaml");
+        $this->assertEquals($ctJson->getDefaultOptions(), $ctYaml->getDefaultOptions());
+        $ctYaml->check();
+        $ctJson->check();
+        /** test with bad schemas */
+        $msg = '';
+        try {
+            $ct = new ConfigTree($this->testDataDir."badSchema1.yaml");
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+        }
+        $this->assertEquals("Bad schema : properties should be an array", $msg);
+        try {
+            $ct = new ConfigTree($this->testDataDir."badSchema2.yaml");
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+        }
+        $this->assertEquals("Bad schema : expecting attributes for property foo", $msg);
+        /** test with options schemas */
+        $ct = new ConfigTree($this->testDataDir."testSchema.yaml", ['subtree2.list1' => ['a', 'b', 'c']]);
+        /** @var array<mixed> $list */
+        $list = $ct->get('subtree2.list1');
+        $this->assertEquals('b', $list[1]);
+    }
+   /**
+     * Test execCommand method
+     *
+     */
+    public function testGetDefaultOptions(): void
+    {
+        $ct = new ConfigTree($this->testDataDir."testSchema.json");
+        $expectedOptions = [
+            'boolean-prop1' => false,
+            'boolean-prop2' => true,
+            'integer-prop1' => 10,
+            'string-prop1'  => null,
+            'string-prop2'  => 'blah blah',
+            'subtree1'      => [
+                'integer-prop1' => 0,
+                'string-prop1'  => 'info',
+            ],
+            'subtree2'      => [
+                'list1'         => [],
+                'boolan-prop'   => false,
+                'subsubtree'    => [],
+            ],
+        ];
+        $this->assertEquals($expectedOptions, $ct->getDefaultOptions());
+        $msg = '';
+        try {
+            $ct = new ConfigTree("absentSchema.json");
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+        }
+        $this->assertEquals("Schema file not found : 'absentSchema.json'", $msg);
+        $msg = '';
+        try {
+            $ct = new ConfigTree($this->testDataDir."unhandledSchema.json");
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+        }
+        $this->assertEquals("Unknown type : 'foo'", $msg);
+    }
+    /**
+     * Test merge and check methods
+     */
+    public function testGetSetMerge(): void
+    {
+        $ct = new ConfigTree($this->testDataDir."testSchema.json");
+        // nominal case : merge exising options
+        $this->assertEquals(false, $ct->get('boolean-prop1'));
+        $this->assertEquals(0, $ct->get('subtree1.integer-prop1'));
+        $ct->merge(['boolean-prop1' => true, 'subtree1.integer-prop1' => 100]);
+        $this->assertEquals(true, $ct->get('boolean-prop1'));
+        $this->assertEquals(100, $ct->get('subtree1.integer-prop1'));
+        // nominal case : get existing option with no default
+        $msg = '';
+        try {
+            $ct->get('subtree2.subsubtree.string');
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+        }
+        $this->assertEquals("Option 'subtree2.subsubtree.string' does not exists or has not been set.", $msg);
+        // nominal case : set option which had no default
+        $ct->set('subtree2.subsubtree.string', 'foo');
+        $this->assertEquals('foo', $ct->get('subtree2.subsubtree.string'));
+        // error case : get option in non existing branch
+        $msg = '';
+        try {
+            $ct->get('foo.bar.baz');
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+        }
+        $this->assertEquals("Config branch '/foo' does not exist.", $msg);
+        try {
+            $ct->get('subtree1.bar.baz');
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+        }
+        $this->assertEquals("Config branch '/subtree1/bar' does not exist.", $msg);
+        // error case : set option in non existing branch
+        try {
+            $ct->set('foo.bar.baz', 1);
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+        }
+        $this->assertEquals("Config branch '/foo' does not exist.", $msg);
+        // error case : set non existing option : this time should fire a check exception
+        $errs = [];
+        try {
+            $ct->set('subtree', 1);
+        } catch (JsonValidationException $e) {
+            $msg = $e->getMessage();
+            $errs = $e->getErrors();
+        }
+        $this->assertEquals("Validation error: config does not match schema.", $msg);
+        $this->assertMatchesRegularExpression("/The property subtree is not defined and /", $errs[0]);
+    }
+}
